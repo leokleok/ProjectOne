6b11713d2a6c4e5eb68a0aa3c42c444ac49ea814
Koh Leok Si <LS@Kohs-MacBook-Air.local>
2016-03-03 18:36:16 +0800

    included timer for game

--- memory.js
+++ memory.js
@@ -1,11 +1,30 @@
 $(document).ready(function()
 {
 
+//easter egg!!!
+var show = false;
+  $(document).keypress(function(event) {
+    if(event.which === 13) {
+      if(!show)
+      {
+        $(".ga").show();
+      }
+      else {
+        $(".ga").hide();
+      }
+      show = !show;
+      // show and !show is a toggle
+    }
+    });
+
+
   $(".newGame").on('click',function()
   {
+    alert ("New Game!");
   document.location.reload(true);
 
    });
+
 
   //listener is my function name
 
@@ -13,15 +32,50 @@ $(document).ready(function()
   var first, firstId; // first refers to first card opened and we use current card to compare with the first card. If first card is unavailable, then current card will be first.
   var numFlipped = 0;
   var score = 0;
+  var player = false;
+  
+  var intervalID;
+  var counter = 30;
+
+
+
+  function startTimer () {
+    $(".timer").html(counter);
+  intervalID = setInterval(timerId, 1000);
+
+
+  function timerId() {
+          counter -= 1;
+         $(".timer").html(counter);
+
+
+         if(counter == 0)
+         {
+           alert("Next Player!");
+           clearInterval(intervalID);
+           player = false;
+          document.location.reload(true);
+         }
+   }
+
+
+  }
+
+
 
   function listener(event)
   {
-
     numFlipped = numFlipped + 1;
     console.log("numFlipped value" + " " + numFlipped);
     console.log(event.target.id);
+    console.log(cardsCreated);
 
-
+    if(!player)
+    {
+      counter = 30;
+      startTimer();
+      player = true;
+    }
 
     // images assigned to each tile, but we're not displaying them yet!
     for (var i = 0; i < cardsCreated.length; i++)
@@ -39,14 +93,25 @@ $(document).ready(function()
         if (numFlipped == 2)
         // when 2 cards are flipped, i want to compare if match or don't match
         {
+
           if (cardsCreated[i].name == first)
           {
             // console.log("true");
             score++;
-            console.log(score);
-             if(score == 8) {
-               alert("You Win")
-             };
+
+            function gamestatus () {
+            if( score == 1) {
+            $(".gamestatus").html("You score " + score + " point");
+               }
+            else if (score == 8 ){
+            $(".gamestatus").html("True Blue Singaporean!");
+              }
+             else {
+            $(".gamestatus").html("You score " + score + " points");
+             }
+           }
+               gamestatus();
+
 
           }
           else
@@ -55,7 +120,7 @@ $(document).ready(function()
 
             function flipDelay()
             {
-              setTimeout(disappear, 1500);
+              setTimeout(disappear, 1300);
             }
 
             function disappear()
@@ -80,6 +145,7 @@ $(document).ready(function()
         }
         else
         {
+
           first = cardsCreated[i].name;
           //firstId is to store info on my first card when open
           firstId = cardsCreated[i].id;
@@ -118,8 +184,8 @@ $(document).ready(function()
     img: "pic/cag.jpg"
   },
   {
-    name: "erp",
-    img: "pic/erp.jpg"
+    name: "durian",
+    img: "pic/durian.jpg"
   },
   {
     name: "esplanade",
@@ -169,6 +235,6 @@ $(document).ready(function()
   {
     cardsCreated[i].id = "S" + (i + 1);
   }
-  // console.log(cardsCreated);
+
 
 });
